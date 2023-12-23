5d208ada09f3c3da3d0cb517e90cf26a929f710b
HARI PRASANTH S <hariprasanth581@gmail.com>
2023-12-24 00:29:09 +0530

    Update footer.js

--- src/componets/footer/footer.js
+++ src/componets/footer/footer.js
@@ -9,12 +9,11 @@ const Footer = () => {
          <p className="font-display px-[5rem] lg:px-[10rem] text-[10px] text-center md:text-[12px] lg:text-lg">Your passport to seamless travel, where every mile is a curated adventure through Ooty's beauty and beyond.</p>
         </aside> 
         <nav className="flex justify-center gap-10 py-5 md:place-self-center md:justify-self-end">
-          <a href='https://www.instagram.com/your_instagram_username' target="_blank" rel="noopener noreferrer"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 50 50">
+          <a href='https://www.instagram.com/dreamdestiny_ooty/' target="_blank" rel="noopener noreferrer"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 50 50">
     <path d="M 16 3 C 8.83 3 3 8.83 3 16 L 3 34 C 3 41.17 8.83 47 16 47 L 34 47 C 41.17 47 47 41.17 47 34 L 47 16 C 47 8.83 41.17 3 34 3 L 16 3 z M 37 11 C 38.1 11 39 11.9 39 13 C 39 14.1 38.1 15 37 15 C 35.9 15 35 14.1 35 13 C 35 11.9 35.9 11 37 11 z M 25 14 C 31.07 14 36 18.93 36 25 C 36 31.07 31.07 36 25 36 C 18.93 36 14 31.07 14 25 C 14 18.93 18.93 14 25 14 z M 25 16 C 20.04 16 16 20.04 16 25 C 16 29.96 20.04 34 25 34 C 29.96 34 34 29.96 34 25 C 34 20.04 29.96 16 25 16 z"></path>
 </svg>
           </a>
-          <a href='https://www.youtube.com/username' target="_blank" rel="noopener noreferrer"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-          <a href='https://www.facebook.com/username' target="_blank" rel="noopener noreferrer"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+          <a href='https://www.facebook.com/dreamdestinycabs' target="_blank" rel="noopener noreferrer"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
         </nav>
         <p className="font-display px-[5rem] lg:px-[10rem] text-[10px] text-center md:text-[12px] lg:text-lg">24,Vishalakshi complex,Upper Bazar,Ooty - 643001</p><br></br>
        <div className="flex justify-center text-[10px]"> <p>Copyright © 2023 - All right reserved</p></div>
