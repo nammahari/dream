74a3257ac81da1137b1b8c08d95798604b7a8142
nammahari <hariprasanth581@gmail.com>
2023-12-24 21:42:11 +0530

    call_us

--- src/componets/navbar/navbar.js
+++ src/componets/navbar/navbar.js
@@ -45,11 +45,14 @@ function Nav() {
             </a>
           </li>
         ))}
-        <a className="md:mt-1 md:mt-1" href="https://wa.me/+919342563416?text=Hello%20I%20Need%20A%20Cab!" target="_blank" rel="noreferrer">
-          <Button>
-            Book Cab
-          </Button>
-        </a>
+        <button onClick={() => window.open('tel:+917867995980')}
+          className="foucs-outline-none bg-[#FCC120] text-white text-lg font-display font-medium px-6 py-3 rounded md:ml-10 hover:bg-[#49AA41] outline-offset-none ring-none md:text-lg"
+        >
+          <div className='flex justify-center flex-row gap-3 items-center '>
+          <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path fill="#ffffff" d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
+          <p>Call Now</p>
+          </div>
+        </button>
       </ul>
     </nav>
   );
